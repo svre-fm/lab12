55449b04ab73f6da95c445cbf30e428f8fb587c1
svre-fm <firmoioi96@gmail.com>
2024-12-24 14:53:42 +0700

    complete 1

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,20 @@
-#include<iostream>
+#include <iostream>
+#include <ctime>
 using namespace std;
 
+long long int fibonacci(int);
+
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
+}
+
+long long int fibonacci(int num){
+    if(num == 0) return 0;
+    else if(num == 1) return 1;
+    else return fibonacci(num-1)+fibonacci(num-2);
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+
+int fibonacci(int num){
